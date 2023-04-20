5b2844deb372e9974da5104450e9843833666dee
edwinmuriithi <kabuimuriithi@gmail.com>
2023-04-20 22:43:21 +0300

    Create dockerfile for backend

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ app.use(cors());
 app.use('/api/products', productRoute)
 
 // Define the PORT
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5002
 
 app.listen(PORT, ()=>{
     console.log(`Server listening on port ${PORT}`)
